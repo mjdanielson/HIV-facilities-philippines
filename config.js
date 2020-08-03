66863759facdb5c7a7f55933ae91a8ab94ca640d
Megan Danielson <50419685+mjdanielson@users.noreply.github.com>
2020-08-03 10:01:29 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,26 @@
+
+const config = {
+    style: "mapbox://styles/mapbox/light-v10",
+    accessToken: "pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2RlcjQxaHMwOW5qMnp0MW9mNXNxMHRzIn0.dAR-7E76V_GBk32jm1Q1ww",
+    CSV: "./Mapbeks.csv",
+    center: [120.982688, 14.643619], //Lng, Lat
+    zoom: 6, //Default zoom
+    title: "HIV Testing Facilities",
+    description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
+    sideBarInfo: ["FACILITY NAME", "STREET ADDRESS", "UPDATED TEL NUMBER"],
+    popupInfo: ["FACILITY NAME"],
+    filters: [
+        {
+            type: "dropdown",
+            title: "Facility Type: ",
+            columnHeader: "TYPE",
+            listItems: [
+                'Treatment Hub',
+                'Identified HIV Testing/Counselling Facility',
+                'Primary HIV Care Facility'
+            ]
+        }
+
+    ]
+
+};
