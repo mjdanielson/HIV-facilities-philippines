7cd8bd7463fd9e66e1a454d835181a10c1c6dd02
mjdanielson <megan.danielson@mapbox.com>
2020-09-09 15:43:11 -0700

    update spreadsheet

--- config.js
+++ config.js
@@ -3,6 +3,8 @@ const config = {
     style: "mapbox://styles/mapbox/light-v10",
     accessToken: "pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2RlcjQxaHMwOW5qMnp0MW9mNXNxMHRzIn0.dAR-7E76V_GBk32jm1Q1ww",
     CSV: "./Mapbeks.csv",
+
+    //1iZ-k7r0KZiQLC2of8QkWBLX0lJZ1WogBkdEKfEEOOV8
     center: [120.982688, 14.643619], //Lng, Lat
     zoom: 8, //Default zoom
     title: "HIV Testing Facilities",
@@ -18,6 +20,17 @@ const config = {
                 'Treatment Hub',
                 'Identified HIV Testing/Counselling Facility',
                 'Primary HIV Care Facility'
+            ]
+        },
+
+        {
+            type: "dropdown",
+            title: "Class: ",
+            columnHeader: "CLASS",
+            listItems: [
+                'Government',
+                'Private',
+                'NGO'
             ]
         }
 
